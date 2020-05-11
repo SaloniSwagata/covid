880a588bafe67ac985f07a15bf9b0b419b5a0f3e
Saloni <saloniswagata@gmail.com>
2020-05-12 00:45:22 +0530

    Added the homescreen

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -11,11 +11,12 @@ class _worldState extends State<world> {
   Widget build(BuildContext context) {
       return new SplashScreen(
       seconds: 5,
-      navigateAfterSeconds: new AfterSplash(),
-      title: new Text("Hang On!!!",
+      navigateAfterSeconds: new Introscreen(),
+      loadingText: new Text("Hang On!!!",
         style: new TextStyle(
           color: Colors.red[700],
           fontWeight: FontWeight.bold,
+          fontFamily: 'Poppins',
           fontSize: 20.0
         ),
       ),
@@ -30,81 +31,219 @@ class _worldState extends State<world> {
   }
 }
 
-class AfterSplash extends StatelessWidget {
+class Introscreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Cool"),
+        title: new Text("COVID-19"),
+        backgroundColor: Colors.deepPurple,
         //automaticallyImplyLeading: false,
         ),
-      body: GridView.count(
-  primary: false,
-  padding: const EdgeInsets.all(40),
-  crossAxisSpacing: 10,
-  mainAxisSpacing: 20,
-  crossAxisCount: 1,
-  children: <Widget>[
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('He\'d have you all unravel at the'),
-      color: Colors.teal[100],
+        drawer: Drawer(
+  child: ListView(
+    // Important: Remove any padding from the ListView.
+    padding: EdgeInsets.zero,
+    children: <Widget>[
+      DrawerHeader(
+        child: Text('COVID-19', 
+        style: TextStyle(
+          color:Colors.white,
+          fontSize: 40,
+        ),),
+        decoration: BoxDecoration(
+          color: Colors.deepPurple,
+        ),
+      ),
+      ListTile(
+        title: Text("India's Status"),
+        onTap: () {
+          // Update the state of the app.
+          // ...
+          Navigator.pop(context);
+        },
+      ),
+      ListTile(
+        title: Text('State Wise Status'),
+        onTap: () {
+          // Update the state of the app.
+          // ...
+          Navigator.pop(context);
+          },
+        ),
+      ],
     ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Heed not the rabble'),
-      color: Colors.teal[200],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Sound of screams but the'),
-      color: Colors.teal[300],
-    ),
-    Container(
-      padding: const EdgeInsets.all(20),
-      child: const Text('Who scream'),
-      color: Colors.teal[400],
-    ),
-   ],
-   ),
-    drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
+  ),
+  body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            DrawerHeader(
-              child: Text('Drawer Header'),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8, 15, 15, 15),
+                    child: Text(
+                      "Symptoms -",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'Poppins'
+                      ),
+                    ),
+                  ),
+                  //SizedBox(height: 20),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        SymptomCard(
+                          image: "assets/images/headache.png",
+                          title: "Headache",
+                          isActive: true,
+                        ),
+                        SymptomCard(
+                          image: "assets/images/cough.png",
+                          title: "Cough",
+                        ),
+                        SymptomCard(
+                          image: "assets/images/fever.png",
+                          title: "Fever",
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8, 20, 20, 20),
+                    child: Text("Prevention - ",
+                     style: TextStyle(
+                       fontSize: 20.0,
+                       fontFamily: 'Poppins'
+                       ),),
+                  ),
+                  PreventCard(
+                    text:
+                        "Since the start of the coronavirus outbreak some places have fully embraced wearing facemasks",
+                    image: "assets/images/wear_mask.png",
+                    title: "Wear face mask",
+                  ),
+                  PreventCard(
+                    text:
+                        "Since the start of the coronavirus outbreak some places have fully embraced wearing facemasks",
+                    image: "assets/images/wash_hands.png",
+                    title: "Wash your hands",
+                  ),
+                  SizedBox(height: 50),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),  
+  );
+  }
+}
+
+class PreventCard extends StatelessWidget {
+  final String image;
+  final String title;
+  final String text;
+  const PreventCard({
+    Key key,
+    this.image,
+    this.title,
+    this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: SizedBox(
+        height: 156,
+        child: Stack(
+          alignment: Alignment.centerLeft,
+          children: <Widget>[
+            Container(
+              height: 136,
+              width: double.infinity,
               decoration: BoxDecoration(
-                color: Colors.blue,
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
               ),
             ),
-            ListTile(
-              title: Text('Item 1'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Item 2'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
+            Image.asset(image, height: 130,),
+            Positioned(
+              left: 130,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                height: 136,
+                width: MediaQuery.of(context).size.width - 170,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        text,
+                        maxLines: 4,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.topRight,
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
     );
   }
-}  
-      
-  
-  
+}
+
+class SymptomCard extends StatelessWidget {
+  final String image;
+  final String title;
+  final bool isActive;
+  const SymptomCard({
+    Key key,
+    this.image,
+    this.title,
+    this.isActive = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      //margin: const EdgeInsets.only(left: 5.0,),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.white,
+      ),
+      child: Column(
+        children: <Widget>[
+          Image.asset(image, height: 90),
+          Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Poppins'),
       home: world(),
       
     );
